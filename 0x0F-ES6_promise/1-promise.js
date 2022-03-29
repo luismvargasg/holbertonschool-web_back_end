0138f0179b387b73ab1b191eaacca2e694325c3b
luismvargasg <1610@holbertonschool.com>
2022-03-29 16:31:57 -0500

    adding task 1

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -1,10 +1,10 @@
 export default function getFullResponseFromAPI(success) {
-  const Promise = new Promise((resolve, reject) => {
+  const myPromise = new Promise((resolve, reject) => {
     if (success) {
       resolve({ status: 200, body: "Success" });
     } else {
       reject(new Error("The fake API is not working currently"));
     }
   });
-  return Promise;
+  return myPromise;
 }
