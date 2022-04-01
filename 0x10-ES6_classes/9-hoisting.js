732c10f674ecb39898381dd81722828bb0b13ac7
luismvargasg <1610@holbertonschool.com>
2022-03-31 19:13:08 -0500

    adding fullstudentdescription method

--- 0x10-ES6_classes/9-hoisting.js
+++ 0x10-ES6_classes/9-hoisting.js
@@ -33,7 +33,7 @@ export class StudentHolberton {
   }
 
   get fullStudentDescription() {
-    return `${self._firstName} ${self._lastName} - ${self._holbertonClass._year} - ${self._holbertonClass._location}`;
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass._year} - ${this._holbertonClass._location}`;
   }
 }
 
