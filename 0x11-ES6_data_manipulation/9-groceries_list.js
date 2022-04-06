503fd7a0740f2e7159be00cc5749b5dcbefa04de
luismvargasg <1610@holbertonschool.com>
2022-04-05 22:01:01 -0500

    adding task 9

---
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,6 @@
+export default function groceriesList() {
+  const list = [['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]];
+  const groceries = new Map();
+  for (const item of list) grocery.set(item[0], item[1]);
+  return groceries;
+}
