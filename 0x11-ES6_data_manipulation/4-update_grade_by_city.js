e7af35470b6d0b6f1f3cc038d524a0a3a56f41d9
luismvargasg <1610@holbertonschool.com>
2022-04-05 21:33:12 -0500

    adding task 4

---
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(list, city, grad) {
+  return list
+    .filter((item) => item.location === city)
+    .map((student) => {
+      const gradeI = grad
+        .filter((i) => i.studentId === student.id)
+        .map((x) => x.grade)[0];
+      const grade = gradeI || 'N/A';
+      return { ...student, grade };
+    });
+}
