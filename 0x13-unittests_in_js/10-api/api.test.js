6e6cf605e3369fc4a323a1b5d375eecf9f144d76
luismvargasg <1610@holbertonschool.com>
2022-05-15 22:36:48 -0500

    fixing task 10

--- 0x13-unittests_in_js/10-api/api.test.js
+++ 0x13-unittests_in_js/10-api/api.test.js
@@ -1,45 +1,45 @@
-const request = require("request");
-const { expect } = require("chai");
+const request = require('request');
+const { expect } = require('chai');
 
-describe("9. Regex integration testing ", function () {
-  it("GET /", function (done) {
+describe('9. Regex integration testing ', function () {
+  it('GET /', function (done) {
     const options = {
-      url: "http://localhost:7865",
-      method: "GET",
+      url: 'http://localhost:7865',
+      method: 'GET',
     };
     request(options, function (err, res, body) {
       expect(res.statusCode).to.equal(200);
-      expect(body).to.be.a("string");
-      expect(body).to.equal("Welcome to the payment system");
+      expect(body).to.be.a('string');
+      expect(body).to.equal('Welcome to the payment system');
     });
     done();
   });
-  it("GET /cart/:id", function (done) {
+  it('GET /cart/:id', function (done) {
     const options = {
-      url: "http://localhost:7865/cart/12",
-      method: "GET",
+      url: 'http://localhost:7865/cart/12',
+      method: 'GET',
     };
     request(options, function (err, res, body) {
       expect(res.statusCode).to.equal(200);
-      expect(body).to.be.a("string");
-      expect(body).to.equal("Payment methods for cart 12");
+      expect(body).to.be.a('string');
+      expect(body).to.equal('Payment methods for cart 12');
     });
     done();
   });
-  it("GET  /cart/:id", function (done) {
+  it('GET  /cart/:id', function (done) {
     const options = {
-      url: "http://localhost:7865/cart/hello",
-      method: "GET",
+      url: 'http://localhost:7865/cart/hello',
+      method: 'GET',
     };
     request(options, function (err, res, body) {
       expect(res.statusCode).to.equal(404);
     });
     done();
   });
-  it("GET /available_payments", function (done) {
+  it('GET /available_payments', function (done) {
     const options = {
-      url: "http://localhost:7865/available_payments",
-      method: "GET",
+      url: 'http://localhost:7865/available_payments',
+      method: 'GET',
     };
     request(options, function (err, res, body) {
       const obj = {
@@ -49,23 +49,24 @@ describe("9. Regex integration testing ", function () {
         },
       };
       expect(res.statusCode).to.equal(200);
-      expect(body).to.be.a("string");
-      expect(JSON.parse(body)).to.be.a("object");
+      expect(body).to.be.a('string');
+      expect(JSON.parse(body)).to.be.a('object');
       expect(JSON.parse(body)).to.deep.equal(obj);
     });
     done();
   });
 
-  it("POST /login", function (done) {
+  it('POST /login', function (done) {
     const options = {
-      url: "http://localhost:7865/login",
+      url: 'http://localhost:7865/login',
       json: true,
       body: {
-        userName: "Betty",
+        userName: 'Betty',
       },
     };
     request.post(options, function (err, res, body) {
       expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome Betty');
     });
     done();
   });
